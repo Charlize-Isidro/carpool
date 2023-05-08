ac05eff588b94ea21b6ece7092b8813255c42b0b
Charlize Isidro <isidroca@students.nu-baliwag.edu.ph>
2023-05-09 00:58:00 +0800

    hosting

--- includes/connection.php
+++ includes/connection.php
@@ -10,7 +10,7 @@ $server = "localhost";
 $server_username = "u235219407_charurot";
 $server_password = "Charlize@15";
 $server_database = "u235219407_charcarpool";
-$home = "http://localhost/carpool1";
+$home = "http://carpool.ka-dahon.shop";
 
 
 $connection = mysqli_connect($server, $server_username, $server_password, $server_database);
